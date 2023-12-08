fd6d50d5a43e8edf42de31958b6a514fcc844915
osman <osmnlhn@gmail.com>
2023-12-08 19:35:58 +0000

    second commit

--- components/Hero.js
+++ components/Hero.js
@@ -7,6 +7,7 @@ export default function Hero() {
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
+           
         </section>
     )
 }
